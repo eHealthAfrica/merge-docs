e733e008a701e8f6238bfc45fa8c7cba65629a3a
Tibor Claassen <tibor@gmah.net>
2015-07-03 17:16:02 +0200

    Track meta data and originals inside merged doc

--- test/merge.js
+++ test/merge.js
@@ -3,6 +3,7 @@ var test       = require('redtape')(beforeEach)
   , sinon      = require('sinon')
   , proxyquire = require('proxyquire')
   , _          = require('lodash')
+  , time       = require('timekeeper')
 
 var merge, uuid
 
@@ -16,8 +17,12 @@ function fakeDoc(props) {
   return { doc: props }
 }
 
-function doc(merged) {
-  return _.omit(merged.doc, '_id')
+function props(doc) {
+  return _.omit(doc.doc, ['_id', 'sources'])
+}
+
+function source(doc) {
+  return _.last(doc.doc.sources)
 }
 
 test('merges input into a single doc', function (t) {
@@ -25,7 +30,7 @@ test('merges input into a single doc', function (t) {
              , fakeDoc({ bar: 'b' })
              ]
   var output = merge(docs)
-  t.deepEqual(doc(output), { foo: 'a', bar: 'b' })
+  t.deepEqual(props(output), { foo: 'a', bar: 'b' })
   t.end()
 })
 
@@ -34,7 +39,7 @@ test('deeply merges properties', function (t) {
              , fakeDoc({ foo: { b: 2 } })
              ]
   var output = merge(docs)
-  t.deepEqual(doc(output), { foo: { a: 1, b: 2 } })
+  t.deepEqual(props(output), { foo: { a: 1, b: 2 } })
   t.end()
 })
 
@@ -43,7 +48,24 @@ test('prefers last property on conflicts', function (t) {
              , fakeDoc({ foo: 'baz' })
              ]
   var output = merge(docs)
-  t.deepEqual(doc(output), { foo: 'baz' })
+  t.deepEqual(props(output), { foo: 'baz' })
+  t.end()
+})
+
+test('creates new id', function (t) {
+  var docs = [ fakeDoc({ _id: '675cb524-d599' })
+             , fakeDoc({ _id: 'be13804d-3b8b' })
+             ]
+  uuid.returns('c7900469-7ede')
+  var output = merge(docs)
+  t.equal(output.doc._id, 'c7900469-7ede')
+  t.end()
+})
+
+test('drops revision', function (t) {
+  var docs = [ fakeDoc({ _rev: '675cb524-d599' }) ]
+  var output = merge(docs)
+  t.notOk(props(output).hasOwnProperty('_rev'))
   t.end()
 })
 
@@ -56,12 +78,20 @@ test('passes along source docs', function (t) {
   t.end()
 })
 
-test('creates new id', function (t) {
-  var docs = [ fakeDoc({ _id: '675cb524-d599' })
-             , fakeDoc({ _id: 'be13804d-3b8b' })
+test('tracks meta data', function (t) {
+  var docs = [ fakeDoc({ foo: 'bar' })
+             , fakeDoc({ foo: 'baz' })
              ]
-  uuid.returns('c7900469-7ede')
-  var output = merge(docs)
-  t.deepEqual(output.doc._id, 'c7900469-7ede')
-  t.end()
+  try {
+    time.freeze(1422880023791)
+    var output = merge(docs)
+    t.deepEqual(source(output), { type: 'migration'
+                                , name: 'merge-docs'
+                                , timestamp: 1422880023791
+                                , docs: [ { foo: 'bar' }, { foo: 'baz' } ]
+                                })
+  } finally {
+    time.reset()
+    t.end()
+  }
 })
